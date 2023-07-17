1c0208d91b15cb4cda69b91c5970ff5a2b0e279b
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-07-17 14:18:25 +0100

    add modular javascript code

---
+++ index.js
@@ -0,0 +1,8 @@
+import { navBarListener } from './module/navbar.js';
+import * as bookController from './module/bookOperations.js';
+
+navBarListener();
+//setInterval(() => timeFunction(), 1000);
+bookController.displayBook();
+bookController.addBookListener();
+bookController.deleteBookListener();
